b41680badf63f881df6a75d4addef46148f91c71
trandtb <trandtb@greenglobal.vn>
2020-03-02 21:42:25 +0700

    init project

---
+++ src/main/java/vn/cal/app/ZkspringApplication.java
@@ -0,0 +1,14 @@
+package vn.cal.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkspringApplication.class, args);
+	}
+
+}
